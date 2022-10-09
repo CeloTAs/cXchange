518a14047c334d4fe99d03675003631ba40c3813
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-10-10 03:18:33 +0400

    Set basic Proxy, Implementation and Factory contracts functions

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -20,7 +20,14 @@ module.exports = {
   defaultNetwork,
   networks: {
     localhost: {
+<<<<<<< HEAD
       url: "http://127.0.0.1:8545"
+=======
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: DEVCHAIN_MNEMONIC
+      }
+>>>>>>> 0217abd (Set basic Proxy, Implementation and Factory contracts functions)
     },
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
