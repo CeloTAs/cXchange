6ffd09f5466505e281afbb730f0fd53fccd86de9
HabibSentongo <habibsentongo@gmail.com>
2022-10-10 04:35:03 +0300

    - Setup MongoDB connection and env.

--- packages/cxchange-api/app.js
+++ packages/cxchange-api/app.js
@@ -1,4 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const mongoose = require('mongoose');
 const express = require('express');
+
+const db_host = process.env.DB_HOST || 'localhost';
+const db_port = process.env.DB_PORT || 27017;
+const db_name = process.env.DB_NAME || 'cxchange';
+mongoose.connect(`mongodb://${db_host}:${db_port}/${db_name}`);
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
 
 const app = express();
 
