0aee0b4d60669d94f5b4818ce6d57a215137d3c9
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-10-11 12:47:45 +0400

    [WIP]: Adding tests

--- packages/hardhat/deploy/00-deploy.js
+++ packages/hardhat/deploy/00-deploy.js
@@ -2,11 +2,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("Greeter", {
-    from: deployer,
-    args: ["hello world"],
-    log: true
-  });
+  // await deploy("Greeter", {
+  //   from: deployer,
+  //   args: ["hello world"],
+  //   log: true
+  // });
 };
 
-module.exports.tags = ["Greeter"];
+// module.exports.tags = ["Greeter"];
