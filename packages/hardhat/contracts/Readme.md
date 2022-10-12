bf653817fef04b77cf0c0148e75a1416ca0559fc
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-10-12 13:21:08 +0400

    Update Readme

--- packages/hardhat/contracts/Readme.md
+++ packages/hardhat/contracts/Readme.md
@@ -3,6 +3,6 @@
 - How do we represent user custodian wallet
   - Approach 1: Seed Phrase (❌ Problem of managing several keys)
   - Contract Wallet (✅ Keep Factory Safe)
-- Create xChangeFactory Contract
+- Create XChangeFactory Contract
 - Create Wallet Implementation Contract
 - Create Wallet Proxy Contract (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/proxy/Proxy.sol)
--- packages/hardhat/deploy/00-deploy.js
+++ packages/hardhat/deploy/00-deploy.js
@@ -1,12 +1,14 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+async function main()  {
+  const Contract = await ethers.getContractFactory("Contract"); 
+  const c = await Contract.deploy(); 
 
-  await deploy("Greeter", {
-    from: deployer,
-    args: ["hello world"],
-    log: true
+  await c.deployed()
+
+  };
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
-};
-
-module.exports.tags = ["Greeter"];
