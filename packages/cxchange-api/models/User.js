fc726893f6d665c6850fc742c71c33591513360e
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-11-01 23:55:22 +0400

    [WIP] Create new user wallet on blockchain, add field uuid to user field

--- packages/cxchange-api/models/User.js
+++ packages/cxchange-api/models/User.js
@@ -1,24 +1,28 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const userSchema = new Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+  fullName: {
+    type: String,
+    required: true
+  },
+  uuid: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model("user", userSchema);
--- packages/cxchange-api/routes/users.js
+++ packages/cxchange-api/routes/users.js
@@ -4,6 +4,23 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 dotenv.config();
 
+// --- Abstraction Starts Here ---
+const { v4: uuidv4 } = require("uuid");
+const { newKit } = require("@celo/contractkit");
+const Web3 = require("web3");
+const web3 = new Web3(process.env.RPC_URL);
+const ContractKit = require("@celo/contractkit");
+const kit = ContractKit.newKitFromWeb3(web3);
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
+const factoryAbi = require("../abis/XChangeFactory.json");
+kit.connection.addAccount(account.privateKey);
+kit.defaultAccount = account.address;
+const factoryContract = new kit.connection.web3.eth.Contract(
+  factoryAbi.abi,
+  process.env.XCHANGE_FACTORY_ADDRESS
+);
+// --- Abstraction End Here ---
+
 const router = express.Router();
 
 // Load Input validations
@@ -18,13 +35,15 @@ router.post("/register", (req, res) => {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ email: req.body.email }).then((user) => {
+  User.findOne({ email: req.body.email }).then(async (user) => {
     if (user) return res.status(400).json({ email: "Email already exists" });
 
+    const uuid = uuidv4();
     const newUser = new User({
       fullName: req.body.fullName,
       email: req.body.email,
       password: req.body.password,
+      uuid: uuid
     });
 
     bcrypt.genSalt(10, (err, salt) => {
@@ -37,9 +56,22 @@ router.post("/register", (req, res) => {
           .catch((err) => console.log(err));
       });
     });
+
+    console.log(newUser.uuid);
+
+    const cUSDcontract = await kit.contracts.getStableToken();
+
+    await factoryContract.methods.newWallet(uuid).send({
+      from: account.address,
+      feeCurrency: cUSDcontract.address,
+      gas: 800000
+    });
+
+    const wallet = await factoryContract.methods.wallets(uuid).call();
+
+    console.log(wallet);
   });
 });
-
 
 // User Login
 router.post("/login", (req, res) => {
@@ -55,7 +87,7 @@ router.post("/login", (req, res) => {
         // jwt payload
         const payload = {
           id: user.id,
-          email: user.email,
+          email: user.email
         };
         // Generate token
         jwt.sign(
@@ -65,7 +97,7 @@ router.post("/login", (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: `Bearer ${token}`,
+              token: `Bearer ${token}`
             });
           }
         );
@@ -73,6 +105,5 @@ router.post("/login", (req, res) => {
     });
   });
 });
-
 
 module.exports = router;
