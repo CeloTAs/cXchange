ac80347313e017baf0b56dc86c729b2c5f73674f
Azeez Abidoye <azeezabidoye@yahoo.com>
2022-11-03 04:14:19 +0100

    .envexample file added

---
+++ packages/fiatConnect-api/src/db/database-config.js
@@ -0,0 +1,9 @@
+const mongoose = require(`mongoose`);
+
+const db_host = process.env.DB_HOST || "127.0.0.1";
+const db_port = process.env.DB_PORT || 27017;
+const db_name = process.env.DB_NAME || "fiat-connect-db";
+
+mongoose.connect(`mongodb://${db_host}:${db_port}/${db_name}`, {
+    useNewUrlParser: true
+});
